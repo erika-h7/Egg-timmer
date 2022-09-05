c3c64686ed1f1db9a212494068ae8d59c810e9a1
erika-h7 <erika.herrera1@outlook.com>
2022-09-05 06:58:44 -0700

    Added an alarm sound when timer is done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
 //    let eggTimes = [
 //        "Soft": 300,
@@ -25,32 +27,53 @@ class ViewController: UIViewController {
         "Hard": 7
     ]
     
-    var secondsRemaining = 60
-    
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer!
+    
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
         timer.invalidate()
         
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
-        print(eggTimes[hardness]!)
-        
         }
     
+    
     @objc func updateTimer(){
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+            
         } else {
+            
+            // Alarm sound
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+            // Timer done
             timer.invalidate()
             titleLabel.text = "Done!"
+            
+            
         }
     }
+    
+    
     
     
     
